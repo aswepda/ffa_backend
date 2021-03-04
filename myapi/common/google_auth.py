78cbf9e1db2e56f9dbf07dccd634ff8875abd7ea
Julian <julian.blazek@outlook.de>
2021-03-04 11:27:59 +0100

    Change Authorization to Header Authorization

--- myapi/common/google_auth.py
+++ myapi/common/google_auth.py
@@ -3,13 +3,16 @@ from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from flask import request, session, g
 import json
+import base64
 
 def google_auth(f):
     @wraps(f)
     def googleAuthFunction(*args, **kwargs):
-        if session.get('googleSession') is not None:
+        if request.headers.get('Authorization') is not None:
+            decodedBytes = base64.b64decode(request.headers.get('Authorization'))
+            decodedStr = str(decodedBytes, 'ascii')
             credentials = Credentials.from_authorized_user_info(
-                json.loads(session['googleSession']))
+                json.loads(decodedStr))
             if credentials and credentials.expired and credentials.refresh_token:
                 credentials.refresh(Request())
             g.googleUser = credentials
--- myapi/resources/auth.py
+++ myapi/resources/auth.py
@@ -1,10 +1,11 @@
 from flask_restful import Resource, abort
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
-from flask import request, session, g
+from flask import g, request
 import os
 import json
 import common.google_auth as auth
+import base64
 
 dirname = os.path.dirname(__file__)
 
@@ -18,11 +19,13 @@ class GoogleAuth(Resource):
             filename, scopes=['openid', 'https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/calendar.events.readonly'],
             redirect_uri='postmessage')
         flow.fetch_token(code=json['code'])
-        session['googleSession'] = flow.credentials.to_json()
-        return {'message': 'Logged In!'}
+        google_credentials = flow.credentials.to_json()
+        encoded_credentials = str(base64.b64encode(google_credentials.encode('ascii')), 'ascii', 'ignore');
+        return {'message': 'Logged In!', 'credentials': encoded_credentials}
 
     @auth.google_auth
     def get(self):
+        print(request.authorization)
         if g.get('googleUser') is None:
             return abort(403, message='Unauthorized!')
         service = build('oauth2', 'v2', credentials=g.get('googleUser'))
