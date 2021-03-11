b185bf5853e8f6d0cc849fa7487db907a708d198
Julian <julian.blazek@outlook.de>
2021-03-11 12:11:01 +0100

    Add Void Cache to Authentication Middleware

--- myapi/common/spotify_auth.py
+++ myapi/common/spotify_auth.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from spotipy.oauth2 import SpotifyOAuth
+from common.spotify_cache_void import CacheVoid
 import spotipy
 from flask import request, g
 
@@ -8,7 +9,7 @@ def spotify_auth(f):
     def spotifyAuthFunction(*args, **kwargs):
         if request.headers.get('Authorization') is not None:
             refresh_code = request.headers.get('Authorization')
-            authObj = SpotifyOAuth(client_id='d5550bed36f64690a6d2ae32d26023bd', client_secret='317ee1ad087448bbb8316ad86a9f39a1', redirect_uri='https://aswepda.surge.sh/#/', open_browser=False)
+            authObj = SpotifyOAuth(client_id='d5550bed36f64690a6d2ae32d26023bd', client_secret='7bb9fade755943888c8e27522498b2ed', redirect_uri='https://aswepda.surge.sh/#/', open_browser=False, cache_handler=CacheVoid())
             accessDict = authObj.refresh_access_token(refresh_code)
             g.spotify = spotipy.Spotify(auth=accessDict['access_token'])
         return f(*args, **kwargs)
