0ca85f2419841ba283a5f0846a2640a116378b29
Luca Fischer <luca.fischer2702@gmail.com>
2021-03-16 13:25:21 +0100

    speech new apikey and url

--- myapi/common/speech_utility.py
+++ myapi/common/speech_utility.py
@@ -3,9 +3,11 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 
 def text_to_speech(text):
-    url = 'https://api.eu-de.text-to-speech.watson.cloud.ibm.com/instances/8f767ae7-66ed-4222-b423-2482e0a25c1f'
-    apikey = 'g72_X_bSr5ksN4E2InJaiRyqBqd_smmmekD59y8iS_0q'
-
+    #url = 'https://api.eu-de.text-to-speech.watson.cloud.ibm.com/instances/8f767ae7-66ed-4222-b423-2482e0a25c1f'
+    #apikey = 'g72_X_bSr5ksN4E2InJaiRyqBqd_smmmekD59y8iS_0q'
+    url = 'https://api.eu-de.text-to-speech.watson.cloud.ibm.com/instances/063159ce-b301-432b-9913-593087aecf25'
+    apikey = 'GJyCioYwg1JzFe6hfzWl5lAa4bGJS3mDXDF3OfNQM4cS'
+    
     # Setup Service
     authenticator = IAMAuthenticator(apikey)
     # New TTS Service
