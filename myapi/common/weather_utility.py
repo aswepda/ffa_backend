0e12bf8311602d1856a7afc9f12e5f37c8debe0e
jannikbuehring <jannik.buehring1@web.de>
2021-03-17 15:05:37 +0100

    finished weather tests & added parameters(now, later, today, tomorrow) for api route

--- myapi/common/weather_utility.py
+++ myapi/common/weather_utility.py
@@ -2,14 +2,19 @@ import configparser
 import requests
 import sys
 
-# https://openweathermap.org/current
-# Current weather
-def get_current_weather_by_city(location):
-    API_KEY = 'bdde23a2384b8b49a5c47d36cbab8780'
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(location, API_KEY)
-    r = requests.get(url)
-    return r.json()
+def get_weather(latitude, longitude, time):
+    if time == 'now':
+        return get_current_weather_by_coordinates(latitude, longitude)
+    elif time == 'later':
+        return get_minutely_weather_forecast_next_hour_by_coordinates(latitude, longitude)
+    elif time == 'today':
+        return get_hourly_weather_forecast_next_48hours_by_coordinates(latitude, longitude)
+    elif time == 'tomorrow':
+        return get_daily_weather_forecast_next_7days_by_coordinates(latitude, longitude)
+    else:
+        return "Not a valid time"
 
+# https://openweathermap.org/current
 # Current weather
 def get_current_weather_by_coordinates(latitude, longitude):
     API_KEY = 'bdde23a2384b8b49a5c47d36cbab8780'
--- myapi/tests/test_weather.py
+++ myapi/tests/test_weather.py
@@ -6,44 +6,25 @@ from myapi import app
 def client():
     return app.app.test_client()
 
-def test_get_current_weather_by_city(client):
-    var = common.weather_utility.get_current_weather_by_city('Stuttgart')
-    response = client.get('/weather')
-    assert response.status_code == 200
-    assert 'cod' in json.loads(response.data)
-    assert json.loads(response.data)['cod'] == 200
-
-# TBD
 def test_get_current_weather_by_coordinates(client):
-    var = common.weather_utility.get_current_weather_by_coordinates('48.7667', '9.1833')
-    response = client.get('/weather')
+    response = client.get('/weather/now?lat=48.7823&lon=9.177')
     assert response.status_code == 200
     assert 'cod' in json.loads(response.data)
     assert json.loads(response.data)['cod'] == 200
 
-# TBD
 def test_get_minutely_weather_forecast_next_hour_by_coordinates(client):
-    var = common.weather_utility.get_current_weather_by_city('Stuttgart')
-    response = client.get('/weather')
+    response = client.get('/weather/later?lat=48.7823&lon=9.177')
     assert response.status_code == 200
-    assert 'cod' in json.loads(response.data)
-    assert json.loads(response.data)['cod'] == 200
+    assert 'minutely' in json.loads(response.data)
 
-# TBD
 def test_get_hourly_weather_forecast_next_48hours_by_coordinates(client):
-    var = common.weather_utility.get_current_weather_by_city('Stuttgart')
-    response = client.get('/weather')
+    response = client.get('/weather/today?lat=48.7823&lon=9.177')
     assert response.status_code == 200
-    assert 'cod' in json.loads(response.data)
-    assert json.loads(response.data)['cod'] == 200
+    assert 'hourly' in json.loads(response.data)
 
-
-# TBD
 def test_get_daily_weather_forecast_next_7days_by_coordinates(client):
-    var = common.weather_utility.get_current_weather_by_city('Stuttgart')
-    response = client.get('/weather')
+    response = client.get('/weather/tomorrow?lat=48.7823&lon=9.177')
     assert response.status_code == 200
-    assert 'cod' in json.loads(response.data)
-    assert json.loads(response.data)['cod'] == 200
+    assert 'daily' in json.loads(response.data)
 
 
