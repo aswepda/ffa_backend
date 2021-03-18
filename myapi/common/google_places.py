499b70e891ace3212d97d655eb6b43448906eccc
r0b42o <hoffmann.rob@gmx.de>
2021-03-18 14:43:14 +0100

    added find place search to existing nearby search, if given coordinates places enter nearby search, if given search srting it enters find place search

--- myapi/common/google_places.py
+++ myapi/common/google_places.py
@@ -3,7 +3,15 @@ import googlemaps
 import pprint
 import time
 
-def google_places(location):
+def google_places(location, search_string):
+    if not search_string:
+        print('true')
+        return get_google_places_nearby(location)
+    else:
+        print('flase')
+        return get_google_places_find_place(search_string)
+
+def get_google_places_nearby(location):
     # Define API Key
     API_KEY = 'AIzaSyBDF9q6ilWVfRnGcxo_18--kfGVQOEu9_o'
 
@@ -12,6 +20,36 @@ def google_places(location):
 
     # Define our Search
     places_result = gmaps.places_nearby(location = location, radius = 1000, open_now = True, type = 'restaurant')
+
+    # Define List
+    places_list = []
+
+    # loop through each place in the results
+    for place in places_result['results']:
+
+        # define my place id
+        my_place_id = place['place_id']
+
+        # define the fields we want sent back to us
+        my_fields = ['name', 'formatted_phone_number', 'type', 'rating', 'price_level', 'photo', 'geometry', 'formatted_address', ]
+
+        # make a request for the details
+        place_details = gmaps.place(place_id = my_place_id, fields = my_fields)
+
+        # append place details to list
+        places_list.append(place_details)
+
+    return places_list
+
+def get_google_places_find_place(search_string):
+    # Define API Key
+    API_KEY = 'AIzaSyBDF9q6ilWVfRnGcxo_18--kfGVQOEu9_o'
+
+    # Define our Client
+    gmaps = googlemaps.Client(key = API_KEY)
+
+    # Define our Search
+    places_result = gmaps.places(query=search_string)
 
     # Define List
     places_list = []
--- myapi/resources/places.py
+++ myapi/resources/places.py
@@ -5,5 +5,5 @@ import common.google_places as places
 
 class Places(Resource):
     def get(self):
-        var = places.google_places('48.7667,9.1833')
+        var = places.google_places('48.7667,9.1833', 'dhbw stuttgart')
         return var
