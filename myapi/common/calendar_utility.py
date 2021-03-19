a4c32a77ca0c8cfd0607f75f2701426a091922d7
Patrick2302 <47270719+Patrick2302@users.noreply.github.com>
2021-03-19 09:17:44 +0100

    Lunchbreak

--- myapi/common/calendar_utility.py
+++ myapi/common/calendar_utility.py
@@ -8,7 +8,6 @@ from google.auth.transport.requests import Request
 import json
 
 
-
 def calendar_events(credentials, time):
     #Save Variables 
     cred=credentials
@@ -38,7 +37,9 @@ def calendar_events(credentials, time):
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
+    #For the get_lunchteam we need the events 
     if time=='lunch':
+        #return needed events
         return events
     else:
         #generate List to store data from events in foor-Loop
@@ -47,7 +48,7 @@ def calendar_events(credentials, time):
         #for-Loop to store eventdata in python-Dict
         for event in events:
            start = event['start'].get('dateTime', event['start'].get('date'))
-           title = event['summary']
+           title = event['summary'] or 'Kein Titel'
            link = event['htmlLink']
            end = event['end'].get('dateTime', event['end'].get('date'))
            informations = {"start": start,
@@ -56,30 +57,78 @@ def calendar_events(credentials, time):
                             "link": link}
            info.append(informations)
 
-    #Add "message" and "speakMessage" to the data
+    #Add "message" and "speakMessage" to the data an return JSON-format
     data = {"message": 'Folgende Termine stehen heute an',
             "data": info,
             "speakMessage": True}
     return data
 
 def get_lunchtime(eventslist):
+    #Define needed Variables
+    counter=0
     starts=[]
     ends=[]
     differences=[]
-    info=[]
+    today=datetime.date.today()
+    minTime=datetime.datetime(today.year, today.month, today.day, 11, 0, 0).isoformat() + 'Z'
+    maxTime=datetime.datetime(today.year, today.month, today.day, 15, 0, 0).isoformat() + 'Z'
 
     # Difference between the amout of events in lunchbreaktime
     # 0 Events means that lunchbreaktime can be at any time
-    if len(eventslist) == 0:
-            data = {"message": 'Du hast heute Nachmittag frei',
+    if eventslist:
+            for event in eventslist:
+                start = event['start'].get('dateTime', event['start'].get('date'))
+                starts.append(start)
+                end = event['end'].get('dateTime', event['end'].get('date'))
+                ends.append(end)
+
+            #Get Duration from 11am to start from first event
+            #Then get Duration from End of the last Event to 3pm
+            difference=datetime.datetime.strptime(starts[0], "%Y-%m-%dT%H:%M:%S+01:00")-datetime.datetime.strptime(minTime, "%Y-%m-%dT%H:%M:%SZ")
+            differences.append(difference.seconds/60)
+            difference=datetime.datetime.strptime(maxTime, "%Y-%m-%dT%H:%M:%SZ")-datetime.datetime.strptime(ends[-1], "%Y-%m-%dT%H:%M:%S+01:00")
+            differences.append(difference.seconds/60)
+
+            #Delete fist start and last end, because they were already used above
+            newStarts=starts.copy()
+            newEnds=ends.copy()
+            del newStarts[0]
+            del newEnds[-1]
+
+            #Get the difference between the start and end times of the not used Events
+            #The first Item in starts is the starttime of the 2nd Event and the last Item in ends is the enddtime of the secondlast Event
+            for starttime in newStarts:
+                difference=datetime.datetime.strptime(starttime, "%Y-%m-%dT%H:%M:%S+01:00")-datetime.datetime.strptime(newEnds[counter], "%Y-%m-%dT%H:%M:%S+01:00")
+                differences.append(difference.seconds/60)
+                counter=counter+1
+
+            #Find largest Breaktime in differences
+            maxBreakTime=max(differences)
+
+            #Get Index of maxTime from differences
+            index=differences.index(maxBreakTime)
+
+            if index==0:
+                startBreak=minTime
+                endBreak=starts[0]
+            elif index==1:
+                startBreak=ends[-1]
+                endBreak=maxTime
+            else:
+                startBreak=ends[index-2]
+                endBreak=starts[index-1]
+
+            info={
+                "start": startBreak,
+                "ende": endBreak,
+                "Dauer": maxBreakTime
+            }
+            data = {"message": 'Deine vorgeschlagene Pausenzeit',
             "data": info,
             "speakMessage": True}
     else:
-        for event in eventslist:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            start.append(start)
-            end = event['end'].get('dateTime', event['end'].get('date'))
-            end.append(end)
+        data = {"message": 'Du hast heute Nachmittag frei',
+            "data": None,
+            "speakMessage": True}
 
-    
     return data
