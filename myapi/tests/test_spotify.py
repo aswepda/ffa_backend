131d2df7b4792ba3c9a3590d380317a19bf37ba0
orifromtheblindforest <mobaben12@bauers-net.de>
2021-03-21 10:54:31 +0100

    Spotify: fixed playGenre, improved test_spotify

--- myapi/tests/test_spotify.py
+++ myapi/tests/test_spotify.py
@@ -1,69 +1,84 @@
-import pytest, json
+import pytest
+import json
 import common.spotify_utility
 from myapi import app
+
 
 @pytest.fixture
 def client():
     return app.app.test_client()
 
-def test_playGenre(client):
-    response = client.get('/spotify/playGenre?genre=rock')
-    assert response.status_code == 200
-    assert 'spotify:playlist:' in json.loads(response.data.uri)
-    assert 'rock' in json.loads(response.data.name)
+
+@pytest.fixture
+def token():
+    token = {
+        # Insert your Spotify TOKEN Here:
+        "Authorization": ""
+    }
+    return token
 
 
-def test_playMusicFromYear(client):
-    response = client.get('/spotify/playYear?year=2012')
+def test_getAlbumFromArtist(client, token):
+    response = client.get(
+        '/spotify/getAlbumFromArtist?album=wish you were here&artist=pink floyd', headers=token)
     assert response.status_code == 200
-    assert 'spotify:playlist:' in json.loads(response.data.uri)
-    assert '2012' in json.loads(response.data.name)
-    
+    assert 'spotify:album:' in str(json.loads(response.data))
+    assert 'Pink Floyd' in str(json.loads(response.data))
+    assert type(json.loads(response.data)) == dict
 
-def test_getFavoriteArtists(client):
-    response = client.get('/spotify/getFavoriteArtists')
-    assert response.status_code == 200
-    assert 'spotify:artist:' in json.loads(response.data)
-    assert type(json.loads(response.data)) == list
-    
 
-def test_getFavoriteGenres(client):
-    response = client.get('/spotify/getFavoriteGenres')
+def test_playGenre(client, token):
+    response = client.get('/spotify/playGenre?genre=rock', headers=token)
     assert response.status_code == 200
-    assert type(json.loads(response.data)) == list
-    
+    assert 'spotify:playlist:' in str(json.loads(response.data))
+    assert 'rock' in str(json.loads(response.data))
 
-def test_getFavoriteTracks(client):
-    response = client.get('/spotify/getFavoriteTracks')
-    assert response.status_code == 200
-    assert 'spotify:track:' in json.loads(response.data)
-    assert type(json.loads(response.data)) == list
-    
-def test_getPlaylists(client):
-    response = client.get('/spotify/getPlaylists?name=workout')
-    assert response.status_code == 200
-    assert 'spotify:playlist:' in json.loads(response.data)
-    assert type(json.loads(response.data)) == list
-    
 
-def test_getUserPlaylists(client):
-    response = client.get('/spotify/getUserPlaylists')
+def test_playMusicFromYear(client, token):
+    response = client.get('/spotify/playYear?year=2012', headers=token)
     assert response.status_code == 200
-    assert 'spotify:playlist:' in json.loads(response.data)
-    assert type(json.loads(response.data)) == list
-    
+    assert 'spotify:playlist:' in str(json.loads(response.data))
+    assert '2012' in str(json.loads(response.data))
 
-def test_getArtist(client):
-    response = client.get('/spotify/getArtist?name=Queen')
-    assert response.status_code == 200
-    assert 'spotify:artist:' in json.loads(response.data)
-    assert json.loads(response.data.name) == 'Queen'
-    assert type(json.loads(response.data)) == list
-    
 
-def test_getAlbumFromArtist(client):
-    response = client.get('/spotify/getAlbumFromArtist?album=wish you were here&artist=pink floyd')
+def test_getFavoriteArtists(client, token):
+    response = client.get('/spotify/getFavoriteArtists', headers=token)
     assert response.status_code == 200
-    assert 'spotify:album:' in json.loads(response.data)
-    assert 'Pink Floyd' in json.loads(response.data[0].artist)
-    assert type(json.loads(response.data)) == list
+    assert 'spotify:artist:' in str(json.loads(response.data))
+    assert type(json.loads(response.data)) == dict
+
+
+def test_getFavoriteGenres(client, token):
+    response = client.get('/spotify/getFavoriteGenres', headers=token)
+    assert response.status_code == 200
+    assert type(json.loads(response.data)) == dict
+
+
+def test_getFavoriteTracks(client, token):
+    response = client.get('/spotify/getFavoriteTracks', headers=token)
+    assert response.status_code == 200
+    assert 'spotify:track:' in str(json.loads(response.data))
+    assert type(json.loads(response.data)) == dict
+
+
+def test_getPlaylists(client, token):
+    response = client.get('/spotify/getPlaylists?name=workout', headers=token)
+    assert response.status_code == 200
+    assert 'spotify:playlist:' in str(json.loads(response.data))
+    assert 'workout' in str(json.loads(response.data))
+    assert type(json.loads(response.data)) == dict
+
+
+def test_getUserPlaylists(client, token):
+    response = client.get('/spotify/getUserPlaylists', headers=token)
+    assert response.status_code == 200
+    assert 'spotify:playlist:' in str(json.loads(response.data))
+    assert type(json.loads(response.data)) == dict
+
+
+def test_getArtist(client, token):
+    response = client.get('/spotify/getArtist?name=Queen', headers=token)
+    assert response.status_code == 200
+    assert 'spotify:artist:' in str(json.loads(response.data))
+    assert 'Queen' in str(json.loads(response.data))
+    assert type(json.loads(response.data)) == dict
