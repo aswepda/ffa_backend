b92c145ac25daa0da19ba2217a568f1a6176bd41
Patrick2302 <patrickhagele@gmail.com>
2021-04-01 10:41:15 +0200

    Added directions API, deleted Google.py

--- myapi/resources/google.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-from flask import g
-import common.google_auth as auth
-
-class Google(Resource):
-    @auth.google_auth
-    def get_events(self):
-        credentials = g.get('googleUser').to_json()
