b341041f80519c4a3a85d7375add5950b1d5debd
jannikbuehring <jannik.buehring1@web.de>
2021-04-12 15:00:51 +0200

    added new tests for weather api

--- myapi/tests/test_weather.py
+++ myapi/tests/test_weather.py
@@ -27,4 +27,12 @@ def test_get_daily_weather_forecast_next_7days_by_coordinates(client):
     assert response.status_code == 200
     assert 'daily' in json.loads(response.data)
 
+def test_get_weather(client):
+    response = client.get('/weather/noow?lat=48.7823&lon=9.177')
+    assert response.status_code == 200
+    assert b"Not a valid time parameter (use 'now', 'later', 'today' or 'tomorrow')" in response.data
 
+def test_get_current_weather_by_coordinates_missing_arguments(client):
+    response = client.get('/weather/now?lat=48.7823')
+    assert response.status_code == 200
+    assert b"Latitude and/or longitude missing" in response.data
